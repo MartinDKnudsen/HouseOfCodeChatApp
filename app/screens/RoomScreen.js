2543c201551e1542adf2023d2ce168b65123ef0c
Martin Knudsen <66463206+MartinDKnudsen@users.noreply.github.com>
2021-01-20 22:11:59 +0100

    Trying out the permission module

--- app/screens/RoomScreen.js
+++ app/screens/RoomScreen.js
@@ -6,7 +6,14 @@ import {
   Send,
   SystemMessage,
 } from 'react-native-gifted-chat'
-import { ActivityIndicator, Icon, StyleSheet, Text, View } from 'react-native'
+import {
+  ActivityIndicator,
+  Icon,
+  PermissionsAndroid,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native'
 import {
   ImagePicker,
   launchCamera,
@@ -22,7 +29,7 @@ import useStatsBar from '../utils/useStatusBar'
 
 export default function RoomScreen({ route }) {
   const [messages, setMessages] = useState([])
-  const [imageUri, setImageUri] = useState()
+  const [filePath, setFilePath] = useState({})
   const { user } = useContext(AuthContext)
   const { chatRoom_id } = route.params
   useStatsBar('light-content')
@@ -42,31 +49,6 @@ export default function RoomScreen({ route }) {
           avatar: user.picture,
         },
       })
-
-    async function handleImageSend(messages) {
-      const image = message[0].image
-      firestore()
-        .collection('Chats')
-        .doc(chatRoom_id)
-        .collection('MESSAGES')
-        .add({
-          image,
-          createdAt: new Date().getTime(),
-          user: {
-            _id: user.name,
-            email: user.email,
-            avatar: user.picture,
-          },
-        })
-    }
-
-    const selectImage = async () => {
-      try {
-        const result = await ImagePicker.launchImageLibraryAsync()
-      } catch (error) {
-        console.log(error)
-      }
-    }
 
     await firestore()
       .collection('Chats')
@@ -175,35 +157,153 @@ export default function RoomScreen({ route }) {
     )
   }
 
-  const handlePickImage = async () => {
-    try {
-      const result = await ImagePicker.launchImageLibrary()
-    } catch (error) {}
-    console.log(result)
+  //******************Handle images  */
+  const requestCameraPermission = async () => {
+    if (Platform.OS === 'android') {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.CAMERA,
+          {
+            title: 'Camera Permission',
+            message: 'App needs camera permission',
+          },
+        )
+        // If CAMERA Permission is granted
+        return granted === PermissionsAndroid.RESULTS.GRANTED
+      } catch (err) {
+        console.warn(err)
+        return false
+      }
+    } else return true
   }
 
-  function handleImgSend(props) {
-    return (
-      <SystemMessage
-        {...props}
-        wrapperStyle={styles.systemMessageWrapper}
-        textStyle={styles.systemMessageText}
-      />
-    )
+  const requestExternalWritePermission = async () => {
+    if (Platform.OS === 'android') {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+          {
+            title: 'External Storage Write Permission',
+            message: 'App needs write permission',
+          },
+        )
+        // If WRITE_EXTERNAL_STORAGE Permission is granted
+        return granted === PermissionsAndroid.RESULTS.GRANTED
+      } catch (err) {
+        console.warn(err)
+        alert('Write permission err', err)
+      }
+      return false
+    } else return true
   }
 
-  function renderActions(props) {
+  const captureImage = async (type) => {
+    let options = {
+      mediaType: type,
+      maxWidth: 300,
+      maxHeight: 550,
+      quality: 1,
+      videoQuality: 'low',
+      durationLimit: 30, //Video max duration in seconds
+      saveToPhotos: true,
+    }
+    let isCameraPermitted = await requestCameraPermission()
+    let isStoragePermitted = await requestExternalWritePermission()
+    if (isCameraPermitted && isStoragePermitted) {
+      launchCamera(options, (response) => {
+        console.log('Response = ', response)
+
+        if (response.didCancel) {
+          alert('User cancelled camera picker')
+          return
+        } else if (response.errorCode == 'camera_unavailable') {
+          alert('Camera not available on device')
+          return
+        } else if (response.errorCode == 'permission') {
+          alert('Permission not satisfied')
+          return
+        } else if (response.errorCode == 'others') {
+          alert(response.errorMessage)
+          return
+        }
+        console.log('base64 -> ', response.base64)
+        console.log('uri -> ', response.uri)
+        console.log('width -> ', response.width)
+        console.log('height -> ', response.height)
+        console.log('fileSize -> ', response.fileSize)
+        console.log('type -> ', response.type)
+        console.log('fileName -> ', response.fileName)
+        setFilePath(response)
+      })
+    }
+  }
+
+  const chooseFile = (type) => {
+    let options = {
+      mediaType: type,
+      maxWidth: 300,
+      maxHeight: 550,
+      quality: 1,
+    }
+    launchImageLibrary(options, (response) => {
+      console.log('Response = ', response)
+
+      if (response.didCancel) {
+        alert('User cancelled camera picker')
+        return
+      } else if (response.errorCode == 'camera_unavailable') {
+        alert('Camera not available on device')
+        return
+      } else if (response.errorCode == 'permission') {
+        alert('Permission not satisfied')
+        return
+      } else if (response.errorCode == 'others') {
+        alert(response.errorMessage)
+        return
+      }
+      console.log('base64 -> ', response.base64)
+      console.log('uri -> ', response.uri)
+      console.log('width -> ', response.width)
+      console.log('height -> ', response.height)
+      console.log('fileSize -> ', response.fileSize)
+      console.log('type -> ', response.type)
+      console.log('fileName -> ', response.fileName)
+      setFilePath(response)
+    })
+  }
+
+  function renderImagePicker(props) {
     return (
       <View style={styles.bottomComponentContainer}>
         <IconButton
           icon="camera"
           size={26}
           color="#0078FF"
-          onPress={handlePickImage}
+          onPress={() => chooseFile('photo')}
         />
       </View>
     )
   }
+
+  function renderCameraCapture(props) {
+    return (
+      <View style={styles.bottomComponentContainer}>
+        <IconButton
+          icon="camera"
+          size={26}
+          color="#0078FF"
+          onPress={() => captureImage('photo')}
+        />
+        <IconButton
+          icon="file"
+          size={26}
+          color="#0078FF"
+          onPress={() => chooseFile('photo')}
+        />
+      </View>
+    )
+  }
+  //******************Handle images*****************
 
   return (
     <GiftedChat
@@ -219,7 +319,7 @@ export default function RoomScreen({ route }) {
       renderBubble={renderBubble}
       renderLoading={renderLoading}
       renderSend={renderSend}
-      renderActions={renderActions}
+      renderActions={renderCameraCapture}
       scrollToBottomComponent={scrollToBottomComponent}
       renderSystemMessage={renderSystemMessage}
       renderScrollComponent
