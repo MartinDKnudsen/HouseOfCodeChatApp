c5efc6bcfa659a841c8d80ce9119a9953ee72f5a
Martin Knudsen <66463206+MartinDKnudsen@users.noreply.github.com>
2021-01-29 15:58:34 +0100

    Cloud function now works

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,7 @@
 module.exports = {
   bracketSpacing: true,
   jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
+  trailingComma: "all",
   printWidth: 80,
   semi: false,
-};
+}
--- functions/index.js
+++ functions/index.js
@@ -1,40 +1,17 @@
-const functions = require('firebase-functions')
-const admin = require('firebase-admin')
-admin.initializeApp()
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
 
-exports.SendNotificationToSubedUsers = functions.firestore
-  .document('Chats/{ChatRoom_id}/MESSAGES')
-  .onCreate((snap, context) => {
-    const ChatRoom_id = context.params.ChatRoom_id
-    // const Message_id = context.params.Message_id
-    const Room_Name = context.params.room_Name
-    const title = context.params.title
-    const message = context.params.text
-    const user = context.params.user._id
-
-    const payload = {
+exports.sendNotificationToTopic = functions.firestore
+  .document("Chats/jem5UU7MS2qOBLTA97jD")
+  .onWrite(async (event) => {
+    const message = {
       notification: {
-        title: "New massage in: '" + Room_Name + "' from " + user,
-        body: message,
-        chatRoom_id: ChatRoom_id,
+        title: "Hey",
+        body: "Works",
       },
-      topic: ChatRoom_id,
-    }
-
-    admin
-      .messaging()
-      .send(payload)
-      .then((response) => {
-        console.log('Successfully sent message:', response)
-      })
-      .catch((error) => {
-        console.log('Error sending message:', error)
-      })
-  })
+      topic: "jem5UU7MS2qOBLTA97jD",
+    };
+    const response = await admin.messaging().send(message);
+    console.log(response);
+  });
